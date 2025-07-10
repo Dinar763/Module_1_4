2244626def3b471bca4cbaa01c933accd0417701
Dinar <gizzyatovdinar@gmail.com>
2025-07-10 18:03:12 +0400

    commit #1

---
+++ src/main/java/homeWork/Foo.java
@@ -0,0 +1,18 @@
+package homeWork;
+
+public class Foo {
+    public void first() {
+        print("first"); 
+    }
+    
+    public void second() {
+        print("second"); 
+    }
+
+    private void print(String second) {
+    }
+
+    public void third() {
+        print("third"); 
+    }
+}
